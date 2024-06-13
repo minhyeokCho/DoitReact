577e6502de5db309a33c938725b0e9d317270609
조민혁 <akf42999@gmail.com>
2024-06-13 18:01:09 +0900

    이벤트 테스트

--- basic_page/src/App.tsx
+++ basic_page/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EventMe from './ch01/EventMe';
+import EventListener from './ch01/EventListener';
+import OnClick from './ch01/OnClick';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+		<OnClick/>
+		{/* <EventListener/> */}
+	</>
   );
 }
 
---
+++ basic_page/src/ch01/EventMe.tsx
@@ -0,0 +1,3 @@
+export default function EventMe() {
+	return <div>test</div>
+}
